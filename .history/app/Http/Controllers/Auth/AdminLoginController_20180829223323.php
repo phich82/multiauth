602dbebab572734e937a83cccacb0e62f97a5695
phich82 <phich82@gmail.com>
2018-08-30 00:54:04 +0700

    first commit

---
+++ .history/app/Http/Controllers/Auth/AdminLoginController_20180829223323.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminLoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+}
---
+++ .history/app/Http/Controllers/Auth/AdminLoginController_20180829225728.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminLoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+        // validate the form data
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // attempt to login
+        if (Auth::guard('admin')->attempt(['email' => $repuest->email, 'password' => $request->password], $request->remember)) {
+            // if successful, redirect to their intended location
+            return redirect()->intended(route('admin.index'));
+        }
+
+        // if unsuccessful, redirect back to the login with form data
+        return redirect()->back()->withInput($request->only(['email', 'remember']));
+    }
+}
