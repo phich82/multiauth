7feb337256dfb89bc5b368936a70473df396c940
phich82 <phich82@gmail.com>
2018-09-05 02:16:40 +0700

    update code

--- .history/resources/views/datepicker.blade_20180903154833.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <p>Date: <input type="text" id="datepicker"></p>
-    </div>
-</div>
-@endsection
--- .history/resources/views/datepicker.blade_20180903155503.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<p>Date: <input type="text" id="datepicker"></p>
-@endsection
