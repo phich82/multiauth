e5c7935cbcf499192c0464c3711b2bebdca8ccd9
phich82 <phich82@gmail.com>
2018-11-11 21:32:29 +0700

    update code

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -9,7 +9,58 @@ class ExcelController extends Controller
 {
     public function exportExcel(ExcelService $excelService)
     {
-        $data = [];
+        $data = [
+            '01-11-2018' =>[
+                ['date' => '01-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+                ['date' => '01-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ],
+            '02-11-2018' => [
+                ['date' => '02-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+                ['date' => '02-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+                ['date' => '02-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ],
+            '03-11-2018' => [
+                ['date' => '03-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+                ['date' => '03-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ],
+            '04-11-2018' =>[
+                ['date' => '04-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ],
+            ['date' => '05-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '06-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '07-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '08-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '09-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '10-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '11-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '12-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '13-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '14-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '15-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '16-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '16-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '17-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '18-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '19-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '20-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '20-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '21-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '22-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '22-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '23-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '24-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '25-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '25-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '26-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '27-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '27-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            '28-11-2018' => [
+                ['date' => '28-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+                ['date' => '28-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ],
+            ['date' => '29-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+            ['date' => '30-11-2018', 'work_type' => 'working', 'project' => 'A', 'start_time' => '08:30', 'end_time' => '17:30', 'remarks' => 'Remark 1'],
+        ];
         $templateExcel = storage_path('templates/timereport.template.xlsx');
 
         return $excelService->exportExcel($data, $templateExcel);
--- app/Services/ExcelService.php
+++ app/Services/ExcelService.php
@@ -2,62 +2,84 @@
 
 namespace App\Services;
 
+use Exception;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
+use PhpOffice\PhpSpreadsheet\Calculation\Functions;
 
 class ExcelService
 {
     public function exportExcel($data, $templateExcel = null)
     {
-        //$spreadsheet = IOFactory::load(storage_path('files/template.xlsx'));
-        //$worksheet = $spreadsheet->getActiveSheet();
+        try {
+            /* redirect output to client browser */
+            // header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            // header('Content-Disposition: attachment;filename="myfile.xlsx"');
+            // header('Cache-Control: max-age=0');
 
-        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-        $spreadsheet = $reader->load(storage_path('files/template.xlsx'));
-        $worksheet = $spreadsheet->getActiveSheet();
+            $spreadsheet = IOFactory::load(storage_path('files/template.xlsx'));
+            $worksheet = $spreadsheet->getActiveSheet();
 
-        //$worksheet->getCell('A1')->setValue('John');
-        //$worksheet->getCell('A2')->setValue('Smith');
+            $startRow = 7;
+            $endRow = 34;
+            $totalRows = $endRow - $startRow + 1;
+            $totalRowsImported = count($data);
+            $totalRowsInserted = 0;
 
-        // add new rows
-        $worksheet->insertNewRowBefore(8, 1);
-        // copy style of this cell to other cell
-        $worksheet->duplicateStyle($worksheet->getStyle('B7'), 'B8:B8');
-        // set data validation for cell
-        $worksheet->getCell('B8')->getDataValidation()
-            ->setType(\PhpOffice\PhpSpreadsheet\Cell\DataValidation::TYPE_LIST)
-            ->setFormula1('Sheet2!$A$2:$A$9')
-            ->setShowDropDown(true);
-//dd($worksheet->getCell('B7')->getDataValidation());
-        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-        $writer->save(storage_path('files/saved/write.xlsx'));
-        /* Here there will be some code where you create $spreadsheet */
-        // redirect output to client browser
-        // header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        // header('Content-Disposition: attachment;filename="myfile.xlsx"');
-        // header('Cache-Control: max-age=0');
+            // add new rows if it is greater than 28
+            if ($totalRowsImported > $totalRows) {
+                $totalRowsInserted = $totalRowsImported - $totalRows;
+                $worksheet->insertNewRowBefore($startRow + 1, $totalRowsInserted);
+            }
 
-        // $spreadsheet = IOFactory::load($templateExcel);
-        // $spreadsheet->getActiveSheet()->insertNewRowBefore(8, 2);
-        //$spreadsheet->getActiveSheet()->appendRow(8, [1, 1, 1, 1, 1]);
-        //$spreadsheet->getActiveSheet()->insertNewRowAfter(8, 2);
-        // $out = [];
-        // foreach ($spreadsheet->getActiveSheet()->getRowIterator() as $row) {
-        //     //$out[] = $row;
-        //     $cells = [];
-        //     $cellIterator = $row->getCellIterator();
-        //     $cellIterator->setIterateOnlyExistingCells(false); // This loops through all cells,
-        //                                                        //    set will be iterated.
-        //     foreach ($cellIterator as $cell) {
-        //         $cells[] = $cell->getValue();
-        //     }
-        //     $out[] = $cells;
-        //     $cells = [];
-        // }
-        // $writer = IOFactory::createWriter($spreadsheet, "Xlsx");
-        // return $writer->save('php://output');
-        //$writer->save("05featuredemo.xlsx");
+            // set values for the specified cells
+            //$worksheet->getCell('A1')->setValue('John');
+            //$worksheet->getCell('A2')->setValue('Smith');
+            //$worksheet->setCellValue('A4', 'Jhp')->setCellValue('A5', 'Phich');
 
-        //dd($writer->save("05featuredemo.xlsx"), $out);
-        //return $templateExcel;
+            $setDataValidation = function ($pCellCoordinate, $rangeData = null) use ($worksheet) {
+                $cell = is_string($pCellCoordinate) ? $worksheet->getCell($pCellCoordinate) : $pCellCoordinate;
+                $cell->getDataValidation()
+                  ->setType(DataValidation::TYPE_LIST)
+                  ->setFormula1($rangeData ?: 'Sheet2!$A$2:$A$9')
+                  ->setShowDropDown(true);
+            };
+
+            // add new rows (1)
+            //$worksheet->insertNewRowBefore(8, 1);
+            // copy style of this cell to other cell
+            //$worksheet->duplicateStyle($worksheet->getStyle('B7'), 'B8:B8'); // B8:B8 ~ B8
+            //dd(Functions::isFormula('F6', $worksheet->getCell('F6')));
+            //$worksheet->setCellValue('F8', $worksheet->getCell('F7'));
+
+            // set the data validation for cells if not exist
+            for ($s = $startRow, $e = $endRow + $totalRowsInserted; $s <= $e; $s++) {
+                $worksheet->getStyle('A'.$s)->getNumberFormat()->setFormatCode('dd/mm/yyy');
+                // set data validation for cell if not exist
+                if (!$worksheet->dataValidationExists('B'.$s)) {
+                    $setDataValidation('B'.$s);
+                }
+            }
+
+            // set the outline border for table
+            $styleArray = [
+                'borders' => [
+                    'outline' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                    ],
+                ],
+            ];
+            $rangeTable = 'A'.($startRow-1).':G'.($endRow + $totalRowsInserted);
+            $worksheet->getStyle($rangeTable)->applyFromArray($styleArray);
+
+            $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+            $writer->save(storage_path('files/saved/output.xlsx'));
+            // return $writer->save('php://output');
+            return ['result' => true];
+        } catch (Exception $e) {
+            dd($e);
+            return $e->getMessage();
+        }
     }
 }
